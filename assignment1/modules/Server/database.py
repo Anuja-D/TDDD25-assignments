44247be452982f884529fe80f3c318d601a934d9
Gyran <Gustav.Ahlberg@gmail.com>
2013-01-24 08:42:25 +0100

    updated assignment 1 with new database.py from assignment 0

--- assignment1/modules/Server/database.py
+++ assignment1/modules/Server/database.py
@@ -19,26 +19,18 @@ class Database(object):
         self.db_file = db_file
         self.rand = random.Random()
         self.rand.seed()
-        self.file = open(self.db_file, "a+")
-        self.file.seek(0)
-        
-        raw_db = self.file.read()
-        self.db = raw_db.split("%\n")
-
-        pass
+        with open(self.db_file, "r") as f:
+            raw_db = f.read()
+        self.db = raw_db.split("%\n")  
 
     def read(self):
         """Read a random location in the database."""
         return self.db[self.rand.randint(0, len(self.db) - 1)]
 
-        pass
-
     def write(self, fortune):
         """Write a new fortune to the database."""
 
-        self.file.write(fortune)
-        self.file.write('\n%\n')
-
+        with open(self.db_file, "a") as f:
+            f.write(fortune)
+            f.write('\n%\n')
         self.db.append(fortune)
-
-        pass
